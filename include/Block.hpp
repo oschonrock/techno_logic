587d9c4cd6e057a9aefa3ca411db9ecbd4bf3aef
Tobias Schonrock <tobias@schonrocks.com>
2024-11-26 23:52:49 +0100

    beginnings of Block structure

--- include/Block.hpp
+++ include/Block.hpp
@@ -3,30 +3,82 @@
 #include <cstddef>
 #include <string>
 
-#include "SFML/Window/Window.hpp"
 #include "SFML/System/Vector2.hpp"
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
 
 #include "details/StableVector.hpp"
 
-enum class Direction{
-  up = 0,
-  down = 1,
-  left = 2,
-  right = 3
+class Block;
+class Gate;
+class Node;
+
+enum struct Direction : std::size_t { up = 0, down = 1, left = 2, right = 3 };
+
+template <typename T>
+concept connectable = std::same_as<T, Block> || std::same_as<T, Gate>;
+
+template <connectable T>
+struct FinalConnection {
+    Ref<T>      ref;
+    std::size_t inputNum;
 };
 
-class Node{
-  sf::Vector2i pos;
-//   std::array<std::variant<>, 4>
+struct EmptyConnection {};
+
+using connection =
+    std::variant<EmptyConnection, Ref<Node>, FinalConnection<Block>, FinalConnection<Gate>>;
+
+enum struct connectionType : std::size_t { Empty = 0, Node = 1, Block = 2, Gate = 3 };
+
+class Node {
+  public:
+    sf::Vector2i pos;
+    // list of connection to nodes/final points. In order of Direction enum ^^
+    std::array<connection, 4> connections;
+
+    unsigned noOfCons() const {
+        unsigned count{};
+        for (const auto& x: connections) {
+            if (connectionType(x.index()) != connectionType::Empty) {
+                ++count;
+            }
+        }
+        return count;
+    }
 };
 
 class Block {
-public:
-  const std::size_t size = 100;
-  std::string name;
-  std::string description;
+    static constexpr float nodeRad = 0.1f;
 
-  void draw(sf::Window &window) {
-    // draw connections, blocks, and gates
-  }
+  public:
+    std::size_t size = 100;
+    std::string name;
+    std::string description;
+
+    StableVector<Node> nodes;
+    //   StableVector<blockInstance> blockInstances;
+    //   StableVector<Gate> gates;
+
+    std::optional<sf::Vector2i> start;
+
+    //   void proposeConnection(sf::Vector2i start, sf::Vector2i end) {
+    //     assert((start.x == end.x) !=
+    //            (start.y == end.y)); // proposed con is vert or hori
+    //   }
+
+    void event(const sf::Event& event, sf::RenderWindow& window, const sf::Vector2f& mousePos) {}
+
+    void frame(sf::RenderWindow& window) {}
+
+    void draw(sf::RenderWindow& window) {
+        auto circ = sf::CircleShape(nodeRad);
+        circ.setOrigin({-nodeRad, -nodeRad});
+        for (const auto& node: nodes) {
+            if (node.obj.noOfCons() != 2) {
+                circ.setPosition(sf::Vector2f(node.obj.pos));
+                window.draw(circ);
+            }
+        }
+    }
 };
